5052bccf2e21f9edbf4cfa305583f556284f14db
sachindra <sachindra.narayan@ticketmaster.com>
2019-11-13 16:10:17 +0530

    Updates

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { LogoImage } from '../style/styled.jsx';
-
 import '../scss/header.scss';
 
 class Header extends Component {
